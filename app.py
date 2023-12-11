5fb5946879daf089978b6b3c142149ab5b23d23e
Gabriel Calcagni <93176365+solidsnk86@users.noreply.github.com>
2023-12-11 03:43:13 -0300

    first commit and upload file

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/scream')
+def scrape():
+    url = 'https://myportfolio-felipe.netlify.app/'
+    options = Options()
+    options.headless = True
+    driver = webdriver.Chrome(options=options)
+
+    try:
+        driver.get(url)
+        driver.implicitly_wait(5)
+
+        html = driver.page_source
+
+        soup = BeautifulSoup(html, 'html.parser')
+
+        titles = [h1.text for h1 in soup.find_all('h1')]
+        articles = [p.text for p in soup.find_all('p')]
+
+        return jsonify({'titles': titles, 'articles': articles})
+
+    finally:
+        driver.quit()
+
+if __name__ == '__main__':
+    app.run(debug=True)
