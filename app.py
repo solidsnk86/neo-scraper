8cec0493f6694e5273d859435794e2283a73e5db
Gabriel Calcagni <93176365+solidsnk86@users.noreply.github.com>
2023-12-13 06:57:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,25 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+import requests
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/scream')
+@app.route('/api/scrape', methods=['GET'])
 def scrape():
-    url = 'https://solidsnk86.netlify.app/'
-    options = Options()
-    options.headless = True
-    driver = webdriver.Chrome(options=options)
-
     try:
-        driver.get(url)
-        driver.implicitly_wait(5)
-
-        html = driver.page_source
-
-        soup = BeautifulSoup(html, 'html.parser')
+        url = 'https://solidsnk86.netlify.app/'
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
 
         titles = [h1.text for h1 in soup.find_all('h1')]
         paragraphs = [paragraphs.text for paragraphs in soup.find_all('p')]
+        list_items = [li.text for li in soup.find_all('li')]
 
-        return jsonify({'titles': titles, 'paragraphs': paragraphs})
-
-    finally:
-        driver.quit()
+        return jsonify({'titles': titles , 'paragraphs': paragraphs , 'list_items': list_items })
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
